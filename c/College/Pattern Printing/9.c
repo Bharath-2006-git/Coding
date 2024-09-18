2d19baeaa9f6a9f19ad382e4b3c8418f486c443e
Bharath-2006-git <bharath2006warrior@gmail.com>
2024-09-18 09:11:41 +0530

    coding

--- c/College/Pattern Printing/9.c
+++ c/College/Pattern Printing/9.c
@@ -58,6 +58,31 @@
 
 
 
+// #include <stdio.h>
+
+// int main() {
+//     int n, i, j, k;
+//     printf("Enter the number of lines: ");
+//     scanf("%d", &n);
+
+    
+//     for (i = 1; i <= n; i++) {
+        
+//         for (j = 1; j <= n - i; j++) {
+//             printf(" ");
+//         }
+    
+//         for (k = 1; k <= 2 * i - 1; k++) {
+//             printf("%d",k);
+//         }
+//         printf("\n");
+//     }
+
+//     return 0;
+// }
+
+
+
 #include <stdio.h>
 
 int main() {
@@ -73,7 +98,7 @@ int main() {
         }
     
         for (k = 1; k <= 2 * i - 1; k++) {
-            printf("%d",k);
+            printf("%d",j);
         }
         printf("\n");
     }
