6cdbfa16908b4bba544f9cb455c2fdd1fca53537
Bharath-2006-git <bharath2006warrior@gmail.com>
2024-10-08 11:12:48 +0530

    coding

---
+++ c/College/Arrays/search.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main() {
+    int n, i;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+    int e[n], x;
+    printf("Enter the elements of the array:\n");
+    for (i = 0; i < n; i++) {
+        scanf("%d", &e[i]);
+    }
+    printf("Enter element to search: ");
+    scanf("%d", &x);
+    for (i = 0; i < n; i++) {
+        if (e[i] == x) {
+            printf("Element found at index %d\n", i);
+            return 0;  
+        }
+    }
+    printf("Element not found\n");
+    return 0;  
+}
