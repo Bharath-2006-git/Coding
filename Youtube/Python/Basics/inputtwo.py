dccb330497550a8bbd003f80be8539f49113b216
Bharath-2006-git <bharath2006warrior@gmail.com>
2024-12-17 15:34:24 +0530

    coding

---
+++ Youtube/Python/Basics/inputtwo.py
@@ -0,0 +1,4 @@
+x,y = map(int,input("Enter two values: ")) # To take without spaces
+# x, y = map(int, input("Enter two integers: ").split()) # To take input separated by space
+print(x+y)
+
---
+++ Youtube/Python/If-Else/01 drive.py
@@ -0,0 +1,6 @@
+a=int(input("Enter your age: "))
+print("Your age is :",a)
+if(a>=18):
+    print("You are allowed to drive")
+else:
+    print("You are not allowed to drive")
