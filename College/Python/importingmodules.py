50ac7c26a7bb38417f95bdcf498b5a4ab6e8a07d
[Bharath-2006-git] <[bharath2006warrior@gmail.com]>
2025-07-17 12:53:01 +0530

    coding

---
+++ College/Python/importingmodules.py
@@ -0,0 +1,2 @@
+import math
+print(math.sqrt(16))  # Output: 4.0
---
+++ College/Python/multiplication.py
@@ -0,0 +1,6 @@
+print("Multiplication Table")
+n=int(input("Enter a number: "))
+i=1
+while(i<=10):
+    print(n,"*",i,"=",n*i)
+    i+=1
